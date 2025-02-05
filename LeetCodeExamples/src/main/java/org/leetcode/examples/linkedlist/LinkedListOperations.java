c62f0abb641ea3ccdf6baaab5ed9a6b6de14dc5b
JeffersonMonteiro <jmonteiro@cusa.com.au>
2025-02-05 15:16:42 +1030

    LinkedList basic operations - part 3

--- LeetCodeExamples/src/main/java/org/leetcode/examples/linkedlist/LinkedListOperations.java
+++ LeetCodeExamples/src/main/java/org/leetcode/examples/linkedlist/LinkedListOperations.java
@@ -5,6 +5,8 @@ package org.leetcode.examples.linkedlist;
 import org.leetcode.examples.linkedlist.model.*;
 import org.leetcode.examples.linkedlist.oldVersion.LinkedList;
 
+import java.util.Stack;
+
 // Given the head of a list, reverse the list and return the
 // head of reversed list
 public class LinkedListOperations {
@@ -238,6 +240,107 @@ public class LinkedListOperations {
         return false;
     }
 
+//    // Function to check if the linked list
+//    // is palindrome or not
+////    The idea is to initialize an empty stack and start traversing from the head node. Push all the nodes
+////    into the stack while traversing till end. Again traverse the linked list and compare the popped node’s data
+////    with the current node data. If both the data are not equal return false, else when stack becomes empty,
+////    it means we reach to the end node. return true, as linked list is a palindrome.
+//    //NAIVE APPROACH
+//    static boolean isPalindrome(Node head) {
+//        Node currNode = head;
+//        Stack<Integer> s = new Stack<>();
+//
+//        // Push all elements of the list to the stack
+//        while (currNode != null) {
+//            s.push(currNode.data);
+//            currNode = currNode.next;
+//        }
+//
+//        // Iterate in the list again and check
+//        // by popping from the stack
+//        while (head != null) {
+//
+//            // Get the topmost element
+//            int c = s.pop();
+//
+//            // Check if data is not same as
+//            // popped element
+//            if (head.data != c) {
+//                return false;
+//            }
+//
+//            // Move ahead
+//            head = head.next;
+//        }
+//
+//        return true;
+//    }
+//
+//
+
+
+    // Function to check whether the list is palindrome
+    static boolean isPalindrome(Node head) {
+        if (head == null || head.next == null)
+            return true;
+
+        Node slow = head, fast = head;
+
+        while (fast.next != null
+                && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        Node head2 = reverseList(slow.next);
+        slow.next = null;
+
+        boolean ret = isIdentical(head, head2);
+
+        head2 = reverseList(head2);
+        slow.next = head2;
+
+        return ret;
+    }
+
+    // Function to check if two lists are identical
+    static boolean isIdentical(Node n1, Node n2) {
+        while (n1 != null && n2 != null) {
+            if (n1.data != n2.data)
+                return false;
+            n1 = n1.next;
+            n2 = n2.next;
+        }
+        return true;
+    }
+
+    // Function to get the intersection point of two linked lists
+    static Node intersectPoint(Node head1, Node head2) {
+
+        // Maintaining two pointers ptr1 and ptr2
+        // at the heads of the lists
+        Node ptr1 = head1;
+        Node ptr2 = head2;
+
+        // If any one of the heads is NULL, there is no intersection
+        if (ptr1 == null || ptr2 == null)
+            return null;
+
+        // Traverse through the lists until both pointers meet
+        while (ptr1 != ptr2) {
+
+            // Move to the next node in each list and if the one
+            // pointer reaches NULL, start from the other linked list
+            ptr1 = (ptr1 != null) ? ptr1.next : head2;
+            ptr2 = (ptr2 != null) ? ptr2.next : head1;
+        }
+
+        // Return the intersection node, or null if no intersection
+        return ptr1;
+    }
+
+
     // This function prints the contents
     // of the linked list starting from the head
     static void printList(Node node) {
@@ -249,18 +352,18 @@ public class LinkedListOperations {
 
     public static void main(String[] args) {
 
-        // Create a hard-coded linked list:
-        // 1 -> 2 -> 3 -> 4 -> 5 -> 6 -> 7
-        Node head = new Node(1);
-        head.next = new Node(2);
-        head.next.next = new Node(3);
-        head.next.next.next = new Node(4);
-        head.next.next.next.next = new Node(5);
-        head.next.next.next.next.next = new Node(6);
-        head.next.next.next.next.next.next = new Node(7);
-
-        System.out.print("Given Linked list:");
-        printList(head);
+//        // Create a hard-coded linked list:
+//        // 1 -> 2 -> 3 -> 4 -> 5 -> 6 -> 7
+//        Node head = new Node(1);
+//        head.next = new Node(2);
+//        head.next.next = new Node(3);
+//        head.next.next.next = new Node(4);
+//        head.next.next.next.next = new Node(3);
+//        head.next.next.next.next.next = new Node(2);
+//        head.next.next.next.next.next.next = new Node(1);
+//
+//        System.out.print("Given Linked list:");
+//        printList(head);
 
 //        // ******INSERTION******
 //        insert(head, 80);
@@ -326,15 +429,43 @@ public class LinkedListOperations {
 //        System.out.print("\n");
 //        printList(head);
 
-        // Create a loop
-        head.next.next.next = head.next;
+//        // Create a loop
+//        head.next.next.next = head.next;
+//
+//        if (detectLoop(head)) {
+//            System.out.print("\n");
+//            System.out.println("true");
+//        } else {
+//            System.out.print("\n");
+//            System.out.println("false");
+//        }
 
-        if (detectLoop(head)) {
-            System.out.print("\n");
-            System.out.println("true");
-        } else {
-            System.out.print("\n");
-            System.out.println("false");
-        }
+//        if (isPalindrome(head)) {
+//            System.out.print("\n");
+//            System.out.println("true");
+//        } else {
+//            System.out.print("\n");
+//            System.out.println("false");
+//        }
+
+        // creation of first list: 10 -> 15 -> 30
+        Node head1 = new Node(10);
+        head1.next = new Node(15);
+        head1.next.next = new Node(30);
+
+        // creation of second list: 3 -> 6 -> 9 -> 15 -> 30
+        Node head2 = new Node(3);
+        head2.next = new Node(6);
+        head2.next.next = new Node(9);
+
+        // 15 is the intersection point
+        head2.next.next.next = head1.next;
+
+        Node intersectionPoint = intersectPoint(head1, head2);
+
+        if (intersectionPoint == null)
+            System.out.println("-1");
+        else
+            System.out.println(intersectionPoint.data);
     }
 }
